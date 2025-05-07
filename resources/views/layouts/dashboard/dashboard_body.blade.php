08beeae4f7e193582d11fac9dd66d41619ddf207
BTSL-Intern <Intern@BTSL-Intern-.btraccl.com>
2025-05-07 12:26:06 +0600

    red

--- resources/views/layouts/dashboard/dashboard_body.blade.php
+++ resources/views/layouts/dashboard/dashboard_body.blade.php
@@ -1,25 +1,11 @@
 <!--begin::Content wrapper-->
-    <div class="d-flex flex-column flex-column-fluid">
-      @include('layouts.dashboard.dashboard_topTitle')
-    </div>
+<div class="d-flex flex-column flex-column-fluid">
+    @include('layouts.dashboard.dashboard_top')
+</div>
 <!--end::Content wrapper-->
-
-<!--begin::Toolbar-->
-    <div id="kt_app_toolbar" class="app-toolbar py-3 py-lg-6">
-      @include('layouts.dashboard.dashboard_topTitle')
-    </div>
-<!--end::Toolbar-->
-
-<!--begin::Content-->
-  <div id="kt_app_content" class="app-content flex-column-fluid">
-    @include('layouts.dashboard.dashboard_topBody')
-  </div>
-  <!--end::Content-->
 
 <!--begin::Footer-->
 <div id="kt_app_footer" class="app-footer">
-      @include('layouts.dashboard.dashboard_footer')
+    @include('layouts.dashboard.dashboard_footer')
 </div>
 <!--end::Footer-->
-
-
---
+++ resources/views/layouts/dashboard/dashboard_top.blade.php
@@ -0,0 +1,11 @@
+<!--begin::Toolbar-->
+<div id="kt_app_toolbar" class="app-toolbar py-lg-6 py-3">
+    @include('layouts.dashboard.dashboard_topTitle')
+</div>
+<!--end::Toolbar-->
+
+<!--begin::Content-->
+<div id="kt_app_content" class="app-content flex-column-fluid">
+    @include('layouts.dashboard.dashboard_topBody')
+</div>
+<!--end::Content-->
