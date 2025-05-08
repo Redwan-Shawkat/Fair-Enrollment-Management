8f16e6ede2487c9effbe13db61cd1493a1b4bf56
BTSL-Intern <Intern@BTSL-Intern-.btraccl.com>
2025-05-08 17:26:55 +0600

    red

---
+++ resources/views/layouts/header/navbar/header-toggle/headertoggle.blade.php
@@ -0,0 +1,6 @@
+<div class="btn btn-flex btn-icon btn-active-color-primary w-30px h-30px" id="kt_app_header_menu_toggle">
+    <i class="ki-duotone ki-element-4 fs-1">
+        <span class="path1"></span>
+        <span class="path2"></span>
+    </i>
+</div>
