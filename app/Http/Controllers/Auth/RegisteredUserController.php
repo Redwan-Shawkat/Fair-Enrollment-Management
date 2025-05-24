29ccfc165d5bfa03185ea3911ec98787963d3ef6
BTSL-Intern <Intern@BTSL-Intern-.btraccl.com>
2025-05-24 11:15:15 +0600

    red

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -45,6 +45,6 @@ class RegisteredUserController extends Controller
 
         Auth::login($user);
 
-        return redirect(route('slayouts.dashboard.dashboard', absolute: false));
+        return redirect(route('layouts.dashboard.dashboard', absolute: false));
     }
 }
